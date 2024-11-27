34418cd7a6dae35d544f26ac3968b7ece255f425
Klismam Pereira <kp.franciosi@gmail.com>
2024-11-27 15:20:25 +0000

    refactor: reduce large lines

--- etl_assignment/general_config.py
+++ etl_assignment/general_config.py
@@ -9,7 +9,7 @@ def get_config() -> tuple[str, int]:
     and returns the values as a list.
 
     Returns:
-        list: A list containing the values TEMP_DIR for the location of the temporary local storage and CHUNK_ZISE for the parsing of data.
+        list: Values TEMP_DIR for temporary local storage and CHUNK_ZISE for the parsing of data.
     """
     config_file = Path("etl_assignment/config/config.json")
     with open(config_file) as json_file:
